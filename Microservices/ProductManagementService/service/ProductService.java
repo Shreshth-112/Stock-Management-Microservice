c54eca357d27ce97c35873700d20c86f7e2035ae
eshwar139 <61374357+eshwar139@users.noreply.github.com>
2024-09-21 16:36:40 +0530

    Product service
    its working

---
+++ Microservices/ProductManagementService/service/ProductService.java
@@ -0,0 +1,72 @@
+package com.product.product.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.product.product.entity.Product;
+import com.product.product.repository.ProductRepository;
+
+@Service
+public class ProductService {
+	
+	@Autowired
+	ProductRepository productRepository;
+	
+	//add products
+	public Product addOrUpdateProduct(Product product) {
+		Optional<Product> existingProduct = productRepository.findByProductName(product.getProductName());
+
+        if (existingProduct.isPresent()) {
+            Product exprod = existingProduct.get();
+            exprod.setQuantity(exprod.getQuantity() + product.getQuantity());
+            return productRepository.save(exprod);
+        } else {
+            return productRepository.save(product);
+        }
+	}
+	
+	//view all products
+	public List<Product> findAll(){
+		return productRepository.findAll();
+	}
+	
+	//filter by id
+	public Product getProductById(long pid) {
+		return productRepository.findById(pid).get();
+	}
+	
+	//filter by name
+	public Optional<Product> getProductByName(String name) {
+		return productRepository.findByProductName(name);
+	}
+	
+	//filter by price
+	public List<Product> getProductByPrice(double price) {
+		return productRepository.findByProductPrice(price);
+	}
+	
+	//filter products less than price
+	public List<Product> getProductLessThan(double price){
+		return productRepository.findByProductPriceLessThan(price);
+	}
+	
+	//update product
+	public Product UpdateProduct(long pid, Product productDetails){
+		Product product = productRepository.findById(pid).get();
+		
+		product.setProductName(productDetails.getProductName());
+		product.setProductPrice(productDetails.getProductPrice());
+		product.setQuantity(productDetails.getQuantity());
+		
+		return productRepository.save(product);
+	}	
+	
+	//delete product
+	public void deleteById(long pid) {
+		productRepository.deleteById(pid);
+	}
+
+}
