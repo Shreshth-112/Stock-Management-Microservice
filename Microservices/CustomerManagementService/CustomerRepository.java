d184e10831e125ba2908535ddc67f6cf35765944
eshwar139 <61374357+eshwar139@users.noreply.github.com>
2024-09-23 22:51:31 +0530

    Delete Microservices/CustomerManagementService directory

--- Microservices/CustomerManagementService/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.customer.customer.CustomerRepository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.customer.customer.CustomersData.CustomerData;
-
-public interface CustomerRepository extends JpaRepository<CustomerData,Long>{
-
-}
